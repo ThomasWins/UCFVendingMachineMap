8d68e6539a100cecaf5cbe3a240d3893702a06b8
xaident <jesuel.rosado01@protonmail.com>
2025-04-09 22:02:21 +0000

    trimmed server.js

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const {MongoClient, ServerApiVersion} = require('mongodb');
+// MongoDB connection
+const url = "mongodb+srv://santycastro2004:jQvXXiFa1oUpqFJF@cluster0.iggc36l.mongodb.net/BigProject?retryWrites=true&w=majority";
 
-const url = "mongodb+srv://santycastro2004:jQvXXiFa1oUpqFJF@cluster0.iggc36l.mongodb.net/BigProject?retryWrites=true&w=majority&appName=Cluster0";
-
-const client = new MongoClient(url, {
-
-	serverApi: ServerApiVersion.v1
-
-});
-
-async function connectDB(){
-
-	try{
-		await client.connect();
-		console.log("Connected to MongoDB!");
-	}catch(err){
-		console.error("Connection Error:", err);
-	}
+async function connectDB() {
+    try {
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB!");
+    } catch (err) {
+        console.error("Connection Error:", err);
+    }
 }
 
 connectDB();
@@ -42,143 +35,17 @@ app.use((req, res, next) => {
     next();
 });
 
-var cardList =
-    [
-        'Roy Campanella',
-        'Paul Molitor',
-        'Tony Gwynn',
-        'Dennis Eckersley',
-        'Reggie Jackson',
-        'Gaylord Perry',
-        'Buck Leonard',
-        'Rollie Fingers',
-        'Charlie Gehringer',
-        'Wade Boggs',
-        'Carl Hubbell',
-        'Dave Winfield',
-        'Jackie Robinson',
-        'Ken Griffey, Jr.',
-        'Al Simmons',
-        'Chuck Klein',
-        'Mel Ott',
-        'Mark McGwire',
-        'Nolan Ryan',
-        'Ralph Kiner',
-        'Yogi Berra',
-        'Goose Goslin',
-        'Greg Maddux',
-        'Frankie Frisch',
-        'Ernie Banks',
-        'Ozzie Smith',
-        'Hank Greenberg',
-        'Kirby Puckett',
-        'Bob Feller',
-        'Dizzy Dean',
-        'Joe Jackson',
-        'Sam Crawford',
-        'Barry Bonds',
-        'Duke Snider',
-        'George Sisler',
-        'Ed Walsh',
-        'Tom Seaver',
-        'Willie Stargell',
-        'Bob Gibson',
-        'Brooks Robinson',
-        'Steve Carlton',
-        'Joe Medwick',
-        'Nap Lajoie',
-        'Cal Ripken, Jr.',
-        'Mike Schmidt',
-        'Eddie Murray',
-        'Tris Speaker',
-        'Al Kaline',
-        'Sandy Koufax',
-        'Willie Keeler',
-        'Pete Rose',
-        'Robin Roberts',
-        'Eddie Collins',
-        'Lefty Gomez',
-        'Lefty Grove',
-        'Carl Yastrzemski',
-        'Frank Robinson',
-        'Juan Marichal',
-        'Warren Spahn',
-        'Pie Traynor',
-        'Roberto Clemente',
-        'Harmon Killebrew',
-        'Satchel Paige',
-        'Eddie Plank',
-        'Josh Gibson',
-        'Oscar Charleston',
-        'Mickey Mantle',
-        'Cool Papa Bell',
-        'Johnny Bench',
-        'Mickey Cochrane',
-        'Jimmie Foxx',
-        'Jim Palmer',
-        'Cy Young',
-        'Eddie Mathews',
-        'Honus Wagner',
-        'Paul Waner',
-        'Grover Alexander',
-        'Rod Carew',
-        'Joe DiMaggio',
-        'Joe Morgan',
-        'Stan Musial',
-        'Bill Terry',
-        'Rogers Hornsby',
-        'Lou Brock',
-        'Ted Williams',
-        'Bill Dickey',
-        'Christy Mathewson',
-        'Willie McCovey',
-        'Lou Gehrig',
-        'George Brett',
-        'Hank Aaron',
-        'Harry Heilmann',
-        'Walter Johnson',
-        'Roger Clemens',
-        'Ty Cobb',
-        'Whitey Ford',
-        'Willie Mays',
-        'Rickey Henderson',
-        'Babe Ruth',
-	'Sir Obama'	    
-    ];
+// Import routes
+const userRoutes = require('./backend/routes/userRoutes');
 
-app.post('/api/addcard', async (req, res, next) => {
-    // incoming: userId, color
-    // outgoing: error
-
-    const { userId, card } = req.body;
-
-    const newCard = { Card: card, UserId: userId };
-    var error = '';
-
-    try {
-        const db = client.db();
-        const result = db.collection('Cards').insertOne(newCard);
-    }
-    catch (e) {
-        error = e.toString();
-    }
-
-    cardList.push(card);
-
-    var ret = { error: error };
-    res.status(200).json(ret);
-});
-
+// Use routes
+app.use('/api/users', userRoutes);
 
 app.post('/api/login', async (req, res, next) => {
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-
+    const { login, password } = req.body;
     var error = '';
 
-    const { login, password } = req.body;
-
-    const db = client.db();
+    const db = mongoose.connection.db;
     const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
 
     var id = -1;
@@ -195,28 +62,6 @@ app.post('/api/login', async (req, res, next) => {
     res.status(200).json(ret);
 });
 
-
-app.post('/api/searchcards', async (req, res, next) => {
-    // incoming: userId, search
-    // outgoing: results[], error
-
-    var error = '';
-
-    const { userId, search } = req.body;
-
-    var _search = search.trim();
-
-    const db = client.db();
-    const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*' } }).toArray();
-
-    var _ret = [];
-    for (var i = 0; i < results.length; i++) {
-        _ret.push(results[i].Card);
-    }
-
-    var ret = { results: _ret, error: error };
-    res.status(200).json(ret);
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
 });
-
-
-app.listen(5000); // start Node + Express server on port 5000
