0965ae88945a07f48e5975a7dbea481909fa3535
Santiago Castro <SantyCastro2004@gmail.com>
2025-04-09 15:26:41 -0400

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,60 +1,69 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-    const [message, setMessage] = useState('');
-    const [loginName, setLoginName] = useState('');
-    const [loginPassword, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [loginName, setLoginName] = useState('');
+  const [loginPassword, setPassword] = useState('');
+  const navigate = useNavigate();
 
+  async function doLogin(event: React.FormEvent) {
+    event.preventDefault();
 
-    async function doLogin(event: any): Promise<void> {
-        event.preventDefault();
+    const obj = { login: loginName, password: loginPassword };
+    const js = JSON.stringify(obj);
 
-        var obj = { login: loginName, password: loginPassword };
-        var js = JSON.stringify(obj);
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        body: js,
+        headers: { 'Content-Type': 'application/json' }
+      });
 
-        try {
-            const response = await fetch('/api/login',
-                { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+      const res = await response.json();
 
-            var res = JSON.parse(await response.text());
-
-            if (res.id <= 0) {
-                setMessage('User/Password combination incorrect');
-            }
-            else {
-                var user = { firstName: res.firstName, lastName: res.lastName, id: res.id }
-                localStorage.setItem('user_data', JSON.stringify(user));
-
-                setMessage('');
-                window.location.href = '/cards';
-            }
-        }
-        catch (error: any) {
-            alert(error.toString());
-            return;
-        }
-    };
-
-    function handleSetLoginName(e: any): void {
-        setLoginName(e.target.value);
+      if (res.id <= 0) {
+        setMessage('User/Password combination incorrect');
+      } else {
+        const user = {
+          firstName: res.firstName,
+          lastName: res.lastName,
+          id: res.id
+        };
+        localStorage.setItem('user_data', JSON.stringify(user));
+        setMessage('');
+        navigate('/cards');
+      }
+    } catch (error) {
+      alert(error.toString());
     }
+  }
 
-    function handleSetPassword(e: any): void {
-        setPassword(e.target.value);
-    }
-
-    return (
-        <div id="loginDiv">
-            <span id="inner-title">PLEASE LOG IN</span><br />
-            <input type="text" id="loginName" placeholder="Username"
-                onChange={handleSetLoginName} /><br />
-            <input type="password" id="loginPassword" placeholder="Password"
-                onChange={handleSetPassword} />
-            <input type="submit" id="loginButton" className="buttons" value="Do It"
-                onClick={doLogin} />
-            <span id="loginResult">{message}</span>
-        </div>
-    );
-};
+  return (
+    <div id="loginDiv">
+      <span id="inner-title">PLEASE LOG IN</span><br />
+      <input
+        type="text"
+        id="loginName"
+        placeholder="Username"
+        onChange={(e) => setLoginName(e.target.value)}
+      /><br />
+      <input
+        type="password"
+        id="loginPassword"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="submit"
+        id="loginButton"
+        className="buttons"
+        value="Do It"
+        onClick={doLogin}
+      />
+      <span id="loginResult">{message}</span>
+    </div>
+  );
+}
 
 export default Login;
