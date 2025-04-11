81cff4fe98417fb4043de0b8902c0b35672871aa
Ken Philip <130425078+k3nph1l1p@users.noreply.github.com>
2025-04-11 18:36:27 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,30 +10,34 @@ const Login = () => {
   return (
     <div className = "flex w-full h-screen">
       <div className = 'w-1/2 h-full'>
-      <img id= "UCFimage" src = {IMAGE} className = "w-full h-full object-cover"/>
+      <img src = {IMAGE} className = "w-full h-full object-cover"/>
      </div>
      <div className = 'w-1/2 h-full bg-white flex flex-col p-20 justify-between'>
      <h1 className = 'text-base text-[#060606]'>GerbertheGoat</h1>
 
-     
-
-     <div className = 'w-full flex flex-col'>
-      <h3 className = 'text-2xl font-semibold mb-4'>Login</h3>
-     </div>
-     <div className = "w-full flex flex-col">
-      <input type = "text"
-      placeholder = "Username"
-      className = "w-full text-black py-4 border-b border-black outline-none focus:outline-none"/>
-      <input type = "password"
-      placeholder = "Password"
-      className = "w-full text-black py-4 border-b border-black outline-none focus:outline-none"/>
-     </div>
-      <div className = "w-full flex flex-col mt-10">
-        <button className = "bg-[#060606] text-white py-4 rounded-md hover:bg-[#060606]">Login</button>
-        <button className = "bg-[#060606] text-white py-4 rounded-md hover:bg-[#060606]">Create Account</button>
+          <div className="w-full flex flex-col mb-6">
+            <h3 className="text-2xl font-semibold mb-4">Login</h3>
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-full text-black py-4 border-b border-black outline-none"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full text-black py-4 border-b border-black outline-none mt-6"
+            />
+          </div>
         </div>
+
+        <div className="w-full">
+          <button className="w-full bg-[#060606] text-white py-4 rounded-md hover:bg-[#333] transition">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
+
 export default Login;
