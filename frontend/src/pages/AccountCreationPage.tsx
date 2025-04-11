dea244c7ee2991a997e11476cc0788f8c6a19611
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-11 18:14:43 -0400

    Create AccountCreationPage.tsx

---
+++ frontend/src/pages/AccountCreationPage.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Login from '../components/AccountCreation.tsx';
+
+const LoginPage = () =>
+{
+
+    return(
+      <div>
+        <PageTitle />
+        <CreateAccount />
+      </div>
+    );
+};
+
+export default LoginPage;
