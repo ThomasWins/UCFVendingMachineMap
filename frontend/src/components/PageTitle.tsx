ff2e0f1ec3177b1dfdd1d74589d2ed86cf386f2b
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-12 13:36:21 -0400

    Update PageTitle.tsx

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,15 +1,6 @@
-import IMAGE from './assets/imagetrial2.png';
-
 function PageTitle()
 {
    return(
-     <div className="w-1/2 h-full">
-        <img
-          src={IMAGE}
-          alt="Login visual"
-          className="w-full h-full object-cover"
-        />
-      </div>
      <h1 id="title">GERBER THE GOAT</h1>
    );
 };
