d08b25ce80f320be0a72e41b25e8971ea745cd2f
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-12 13:49:41 -0400

    Update App.tsx
    Added 2 new branches /home and /map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage'
 import AccountCreationPage from './pages/AccountCreationPage';
+import HomePage from './pages/HomePage';
+import MapPage from './pages/MapPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/CreateAccount" element={<AccountCreationPage />} />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/Map" element={<MapPage />} />
       </Routes>
     </Router>
   );
