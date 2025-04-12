f0f33eb9b44a37173fb4bd2626c8449375f67fa8
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-12 13:40:02 -0400

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,17 @@
 import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
+import IMAGE from './assets/imagetrial2.png';
 
 const LoginPage = () =>
 {
 
     return(
       <div>
+        <img
+        src={IMAGE}
+        alt="Login Visual"
+        style={{ width: '200px', margin: '20px auto', display: 'block' }}
+        />
         <PageTitle />
         <Login />
       </div>
