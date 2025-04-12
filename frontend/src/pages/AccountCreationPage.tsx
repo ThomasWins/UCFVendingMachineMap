228e70030bf2045cf4f5e2d15be07aded8b77b99
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-12 13:20:25 -0400

    Update AccountCreationPage.tsx
    import CreateAccount from '../components/CreateAccount.tsx';

--- frontend/src/pages/AccountCreationPage.tsx
+++ frontend/src/pages/AccountCreationPage.tsx
@@ -1,5 +1,5 @@
 import PageTitle from '../components/PageTitle.tsx';
-import Login from '../components/AccountCreation.tsx';
+import CreateAccount from '../components/CreateAccount.tsx';
 
 const LoginPage = () =>
 {
