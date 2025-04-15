672c02b52a0076d653b56c7f82c81d3e8f8903ee
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-15 12:39:44 -0400

    Update userRoutes.js
    added: router.post('/logout', userController.logoutUser);

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
+router.post('/logout', userController.logoutUser);
 router.post('/favorites/add', userController.addFavorite);
 router.post('/favorites/remove', userController.removeFavorite);
 
