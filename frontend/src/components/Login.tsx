7a2c0ebc1e0c1f070f569a9b0412ef8d4de302d6
xaident <jesuel.rosado01@protonmail.com>
2025-04-15 00:36:00 +0000

    attempting to hook up login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
     const js = JSON.stringify(obj);
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://gerberthegoat.com/api/users/login', {
         method: 'POST',
         body: js,
         headers: { 'Content-Type': 'application/json' }
