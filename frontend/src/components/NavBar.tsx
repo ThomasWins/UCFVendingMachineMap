a1d077b0a9d3799569a78bf559886b5aec521177
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-16 11:33:23 -0400

    Update NavBar.tsx

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './CSS/navBar.css';
 
-const NavBar: React.FC = () => {
+interface NavBarProps {
+  onAboutClick: () => void;
+}
+
+const NavBar: React.FC<NavBarProps> = ({ onAboutClick }) => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -10,8 +14,8 @@ const NavBar: React.FC = () => {
       </div>
       <ul className="navbar-links">
         <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li id ="machine"><Link to="/vendingrequest">Add Machine</Link></li>
+        <li><button onClick={onAboutClick}>About</button></li>
+        <li id="machine"><Link to="/vendingrequest">Add Machine</Link></li>
         <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/logout">Logout</Link></li>
       </ul>
