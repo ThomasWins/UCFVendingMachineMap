e78a951194a8b335e053c0e95fe34c170b43289e
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-16 11:31:29 -0400

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,20 @@
+import React, { useRef } from 'react';
 import NavBar from '../components/NavBar';
 import HomeBody from '../components/HomeBody';
 
+const HomePage = () => {
+  const aboutRef = useRef(null);
 
-const HomePage = () =>
-{
-    return(
-        <div>
-            <NavBar />
-            <HomeBody />
-        </div>
-    );
-}
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <NavBar onAboutClick={scrollToAbout} /> 
+      <HomeBody aboutRef={aboutRef} />
+    </div>
+  );
+};
 
 export default HomePage;
