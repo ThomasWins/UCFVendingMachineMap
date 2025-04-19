834d1ff8be4d21aadf6eec4c89aba107996ba75d
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-19 14:31:55 -0400

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
 import NavBar from '../components/NavBar';
 import HomeBody from '../components/HomeBody';
 import Footer from '../components/Footer';
+
 const HomePage = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'auto';
+  }, []);
 
 return (
   <div>
