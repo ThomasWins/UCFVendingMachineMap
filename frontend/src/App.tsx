5e69b14089be967875d7830ddadbcbc4f0df3b08
Santiago Castro <55555103+Santy37@users.noreply.github.com>
2025-04-19 15:37:23 -0400

    This is not permanent will revert

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,8 @@ function App() {
   return (
     <Router >
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} /> {/* Changed to HomePage */}
+        <Route path="/login" element={<LoginPage />} /> {/* Added login route */}
         <Route path="/createAccount" element={<AccountCreationPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/map" element={<MapPage />} />
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -21,21 +21,21 @@ const NavBar: React.FC = () => {
         <Link to="/secret">GerberTheGoat</Link>
       </div>
       <ul className="navbar-links">
-        <li><a href="/home#">Home</a></li>
-        <li><a href="/home#aboutContainer">About</a></li>
+        <li><Link to="/">Home</Link></li> {/* Changed from anchor to Link */}
+        <li><a href="/#aboutContainer">About</a></li> {/* Changed to root path with hash */}
 
         {isLoggedIn ? (
           <>
             <li id="machine"><Link to="/map" state={{ openPopup: true }}>Add Machine</Link></li>
             <li><Link to="/profile">Profile</Link></li>
-            <li className="separator">|</li> {/* separator bar */}
+            <li className="separator">|</li>
             <li><Link to="/logout">Logout</Link></li>
           </>
         ) : (
           <>
-            <li id="machine"><Link to="/">Add Machine</Link></li>
-            <li><Link to="/">Login</Link></li>
-            <li className="separator">|</li> {/* separator bar */}
+            <li id="machine"><Link to="/login">Add Machine</Link></li> {/* Changed to /login */}
+            <li><Link to="/login">Login</Link></li> {/* Changed to /login */}
+            <li className="separator">|</li>
             <li id="last"><Link to="/createAccount">Sign Up</Link></li>
           </>
         )}
