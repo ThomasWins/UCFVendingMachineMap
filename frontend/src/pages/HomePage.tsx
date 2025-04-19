48d1e43ab38db5df3245de0e7290621b34818e5a
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-19 13:17:17 -0400

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
 import NavBar from '../components/NavBar';
 import HomeBody from '../components/HomeBody';
 import Footer from '../components/Footer';
-import LoggedInName from '../components/LoggedInName';
-
 const HomePage = () => {
 
 return (
   <div>
     <NavBar /> 
-    <LoggedInName />
     <HomeBody />
     <div id="footerContainer">
           <Footer />
