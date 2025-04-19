830254140f047451d3ab9daca3dec01c472f4d39
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-19 11:30:21 -0400

    Update MapPage.tsx

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,8 +1,21 @@
 import MapComponent from '../components/MapComponent';
 import React from 'react';
 
-const MapPage: React.FC = () => {
+import React, { useEffect } from 'react';////////////////
+import { useLocation } from 'react-router-dom';////////////////
 
+const MapPage: React.FC = () => {
+////////////////////////////////////////////////
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.openPopup) {
+      // Open popup logic here
+      console.log('Popup should open');
+    }
+  }, [location]);
+////////////////////////////////////////////////
+  
 return (
   <div id="mapPage">
     <MapComponent /> 
