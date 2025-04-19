c0796689aea69c0734ecc1ba4ce8b71d680c99a6
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-19 13:20:00 -0400

    Update NavBar.tsx

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import './CSS/navBar.css';
 
 const NavBar: React.FC = () => {
+  const _ud = localStorage.getItem('user_data');
+  let isLoggedIn = false;
+
+  try {
+    const userData = _ud ? JSON.parse(_ud) : null; 
+    if (userData && userData.id) {
+      isLoggedIn = true;
+    }
+  } catch (e) {
+    console.error("Error parsing user_data:", e);
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -11,9 +23,19 @@ const NavBar: React.FC = () => {
       <ul className="navbar-links">
         <li><a href="/home#">Home</a></li>
         <li><a href="/home#aboutContainer">About</a></li>
-        <li id ="machine"><Link to="/map" state={{ openPopup: true}}>Add Machine</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
+        <li id="machine"><Link to="/map" state={{ openPopup: true }}>Add Machine</Link></li>
+
+        {isLoggedIn ? (
+          <>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/logout">Logout</Link></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+          </>
+        )}
       </ul>
     </nav>
   );
