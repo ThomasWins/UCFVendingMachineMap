857fc175a6ff7b50cb690a338c2a4f2d24a6a52d
Michael <mbiskup2608@gmail.com>
2025-04-21 17:26:20 -0400

    Update MapComponent.tsx

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -536,7 +536,8 @@ return (
     <div className={`favorites-popup ${isFavoritesOpen ? 'open' : ''}`}>
       <button className="favorites-close-button" onClick={toggleFavorites}>×</button>
       <h2>Favorites</h2>
-
+     console.log('User Favorites:', userData?.favorites);
+     console.log('Vending Data:', vendingData);
       {userData?.Favorites?.length === 0 ? (
   <p>No favorites yet. Favorite a vending machine to see it here.</p>
 ) : (
