bd60b42815bac2ffa1604cc2e01efc96c1ee7d51
xaident <jesuel.rosado01@protonmail.com>
2025-04-21 23:38:26 +0000

    updated server.js, basically moved multer logic into a separate file

--- server.js
+++ server.js
@@ -55,37 +55,9 @@ const vendingRoutes = require('./backend/routes/vendingRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/vending', vendingRoutes);
 
+// Image uploads
+app.use('/uploads', express.static('uploads'));
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); 
-    },
-    filename: (req, file, cb) => {
-      
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
-  });
-  
-  // File filter to only accept images
-  const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-      cb(null, true);
-    } else {
-      cb(new Error('Not an image! Please upload an image.'), false);
-    }
-  };
-  
-  const upload = multer({ 
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-      fileSize: 5 * 1024 * 1024 // 5MB limit
-    }
-  });
-  
-  
-  app.use('/uploads', express.static('uploads'));
