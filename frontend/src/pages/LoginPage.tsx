0826d217ab9629f1249409cd9925ad04e603b13c
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-21 15:44:23 -0400

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
 import IMAGE from '../assets/vendingmachinetrial.jpg';
@@ -9,23 +10,35 @@ import NavBar from '../components/NavBar.tsx';
 import Footer from '../components/Footer.tsx';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const _ud = localStorage.getItem('user_data');
+    try {
+      const userData = _ud ? JSON.parse(_ud) : null;
+      if (userData && userData.userId) {
+        navigate('/home');
+      }
+    } catch (e) {
+      console.error('Error parsing user_data:', e);
+    }
+  }, []);
 
   return (
     <div id="LoginBody">
       <NavBar />
-  <div className="container">
-    <div className="left">
-      <img src={IMAGE} alt="Login visual" />
+      <div className="container">
+        <div className="left">
+          <img src={IMAGE} alt="Login visual" />
+        </div>
+        <div id="loginForm">
+          <PageTitle />
+          <Login />
+        </div>
+      </div>
+      <Footer />
     </div>
-    <div id="loginForm">
-      <PageTitle />
-      <Login />
-    </div>
-  </div>
-  <Footer />
-</div>
   );
-  }
+};
+
 export default LoginPage;
-
-
