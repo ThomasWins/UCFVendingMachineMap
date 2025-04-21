6810e2b93742b775d3d22d277e359fab38c1d3ed
xaident <jesuel.rosado01@protonmail.com>
2025-04-21 01:06:56 +0000

    comments

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   login: { type: String, required: true, unique: true },
-  password: { type: String, required: true },  // Store passwords as plain text for now
+  password: { type: String, required: true },
   userId: { type: Number, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
