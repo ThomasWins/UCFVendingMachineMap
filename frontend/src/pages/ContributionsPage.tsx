9f5197938020e5bc8a7100c0d25ee9eecd06545e
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-22 12:57:32 -0400

    Create ContributionsPage.tsx

---
+++ frontend/src/pages/ContributionsPage.tsx
@@ -0,0 +1,14 @@
+import NavBar from '../components/NavBar';
+import ContributionsBody from '../components/ContributionsBody';
+
+const ContributionsPage = () => {
+
+return (
+  <div>
+    <NavBar /> 
+    <ContributionsBody />
+  </div>
+  );
+};
+
+export default contributionsPage;
