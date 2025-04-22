57ff8c02e0eda1af85ccda0fb0e26cd4abd15739
Ken Philip <130425078+k3nph1l1p@users.noreply.github.com>
2025-04-22 10:18:10 -0400

    Added back redirect to profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ function App() {
         <Route path="/logout" element={<Logout />} />
         <Route path="/secret" element={<TheGoat />} />
         <Route path="/admin" element={<AdminPage/>} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
