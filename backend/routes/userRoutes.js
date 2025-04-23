513167b3d8cfdd041552b065b1b176a7ff1728ad
xaident <jesuel.rosado01@protonmail.com>
2025-04-23 21:14:10 +0000

    reorganized userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,27 +8,21 @@ router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.post('/logout', userController.logoutUser);
 
-// User's favorites
-router.post('/favorites/add', userController.addFavorite);
-router.get('/:userId/favorites', userController.retrieveFavorites);
-router.delete('/:userId/favorites/:vendingId', userController.removeFavorite);
-
-// User's profile
+// User's Profile/Info
 router.get('/profile/:userId', userController.getUserProfile);
-
-// User's info
 router.get('/info/:userId', userController.getUserInfo);
 
-// User's vending machine request submission
-router.post('/:userId/vending-requests', userController.submitVendingRequest);
+// User's Add/Delete/Retrieve Favorites
+router.post('/favorites/add', userController.addFavorite);
+router.delete('/:userId/favorites/:vendingId', userController.removeFavorite);
+router.get('/:userId/favorites', userController.retrieveFavorites);
 
-// User's contributions
+// User's Requests/Contributions
+router.post('/:userId/vending-requests', userController.submitVendingRequest);
 router.get('/:userId/contributions', userController.getUserContributions);
 
-// Get all vending machine requests (admin only)
+// Admin Retrieve/Update Requests
 router.get('/:userId/vending-requests', userController.getVendingRequests);
-
-// Update a vending machine's request status (admin only)
 router.patch('/:userId/vending-requests/:requestId', userController.updateVendingRequest);
 
 module.exports = router;
