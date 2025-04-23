2b778f28249d24b4129d3701cbc22a59a290a78d
Santiago Castro <SantyCastro2004@gmail.com>
2025-04-23 18:36:16 -0400

    Tryna fix images to show

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -577,12 +577,13 @@ return (
       <div className="vending-popup">
         {/*HARDCODED IMAGE NEEDS TO BE CHANGED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
         {selectedVending.imageUrl && (
-          <img
-            src={selectedVending.imageUrl}
-            alt={selectedVending.name}
-            className="vending-popup-image"
-          />
-        )}
+  <img
+    src={`https://gerberthegoat.com/${selectedVending.imageUrl}`}
+    alt={selectedVending.name}
+    className="vending-popup-image"
+    onError={(e) => console.error("Image load error:", e)}
+  />
+)}
         <div className="vending-popup-content">
           <button className="close-vending-popup" onClick={() => setIsVendingPopupOpen(false)}>×</button>
           <div className="vending-title">{selectedVending.name}</div>
--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -77,7 +77,7 @@ const handleAction = async (status: 'approved' | 'rejected') => {
         type: selectedRequest.type,
         lat: selectedRequest.coordinates.latitude,
         lng: selectedRequest.coordinates.longitude,
-        imageUrl: selectedRequest.imagePath || null, 
+        imageUrl: selectedRequest.imagePath, 
       };
 
       const uploadRes = await fetch('https://gerberthegoat.com/api/vending/manual', {
