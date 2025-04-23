24ea6181668175fcfe05d6ee47761163c21faa1d
Santiago Castro <SantyCastro2004@gmail.com>
2025-04-23 19:50:23 -0400

    Revert "more fixing and debugging im trying man"
    This reverts commit 311373cbb53be9904ca4018e7eb95c5ceca9eb15.

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -150,11 +150,6 @@ useEffect(() => {
 
 // goes through the vending machines in the database and will add markers at the coordinates
 const renderMarkers = (mapInstance: mapboxgl.Map) => {
-  console.log("Vending data with images:", vendingData.map(item => ({
-    id: item.id, 
-    name: item.name,
-    imageUrl: item.imageUrl
-  })));
 
   const existingMarkers = document.querySelectorAll('.mapboxgl-marker');
   existingMarkers.forEach(marker => marker.remove());
@@ -583,15 +578,10 @@ return (
         {/*HARDCODED IMAGE NEEDS TO BE CHANGED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
         {selectedVending.imageUrl && (
           <img
-            src={selectedVending.imageUrl.startsWith('http') 
-              ? selectedVending.imageUrl 
-              : `https://gerberthegoat.com/${selectedVending.imageUrl}`}
+            src={`https://gerberthegoat.com/${selectedVending.imageUrl}`}
             alt={selectedVending.name}
             className="vending-popup-image"
-            onError={(e) => {
-              console.error("Image load error:", e.target.src);
-              e.target.src = "https://gerberthegoat.com/placeholder.png"; // Fallback image
-            }}
+            onError={(e) => console.error("Image load error:", e.target.src)}
           />
         )}
         <div className="vending-popup-content">
