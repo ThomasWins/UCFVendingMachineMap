4c8913ac4050732c4e3352e190cc0727d2b4179d
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-24 12:15:17 -0400

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: React.FC = () => {
           <h4>Links</h4>
           <ul>
             <li><a href="#aboutContainer">About</a></li>
-            <li><a href="#team">Meet the Team</a></li>
+            <li><Link to="/profile">Profile</a></li>
             <li><Link to="/map" state={{ openPopup: true}}>Add a Machine</Link></li>
           </ul>
         </div>
@@ -30,22 +30,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="footer-section social-icons">
-          <h4>Follow Us</h4>
-          <a href="https://www.x.com" target="_blank" rel="noopener noreferrer">
-            <img src={twitterLogo}
-              alt="Twitter"
-              style={{ width: '24px', height: '24px' }} />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src={instagramLogo}
-              alt="Instagram"
-              style={{ width: '24px', height: '24px' }} />
-          </a>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src={facebookLogo}
-              alt="Facebook"
-              style={{ width: '24px', height: '24px' }} />
-          </a>
+          // No reason to add this
         </div>
 
       </div>
