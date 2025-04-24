9f6dd6fc4f7301fffbe3cda754fdddfadc62298b
Michael <mbiskup2608@gmail.com>
2025-04-24 13:07:55 -0400

    Create vendingFallback.js

---
+++ frontend/src/components/vendingFallback.js
@@ -0,0 +1,40 @@
+import msb from './photos/msb.jpg';
+import fc from './photos/FerrelCommons.jpg';
+import rwc from './photos/rwc.png';
+import tennis from './photos/TenisCourt.jpg';
+import eng1 from '/photos/eng1.png';
+import eng2 from '/photos/eng2.png';
+import cb1 from './photos/cb1.png';
+import cb2 from './photos/cb2.png';
+import creol from './photos/creol.png';
+import hec1 from './photos/hec1.png';
+import hec2 from './photos/hec2.png';
+import hs1 from './photos/hs1.png';
+import hs2 from './photos/hs2.png';
+import library from './photos/library.png';
+import nsc from './photos/nsc.png';
+import outside from './photos/outside.png';
+import psy from './photos/psy.png';
+import research1 from './photos/research1.png';
+import su1 from './photos/su1.png';
+
+
+export const fallbackImages = {
+  '1': msb,       
+  '2': fc,          
+  '3': rwc,           
+  '4': tennis,                 
+  '7': eng1,           
+  '8': psy,          
+  '9': cb2,      
+  '10': cb1,         
+  '11': nsc,     
+  '12': creol,         
+  '13': research1,    
+  '14': hec1,
+  '15': eng2,
+  '16': su1,
+  '17': library,
+  '18': hs1,
+  '19': hs2,
+};
