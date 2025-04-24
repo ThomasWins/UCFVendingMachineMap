f48eab4e64dbf3764c22bc2878b6cb586ffe605d
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-04-24 12:18:37 -0400

    Update MapPage.tsx

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import MapComponent from '../components/MapComponent';
 
 const MapPage: React.FC = () => {
@@ -7,21 +7,6 @@ const MapPage: React.FC = () => {
   const [isVendingRequestPopupOpen, setIsVendingRequestPopupOpen] = useState(false);
 
   useEffect(() => {
-
-  const _ud = localStorage.getItem('user_data');
-    try {
-      const parsedData = _ud ? JSON.parse(_ud) : null;
-      if (parsedData && parsedData.userId) {
-        setUserData({ firstName: parsedData.firstName, lastName: parsedData.lastName });
-      } else {
-        navigate('/');
-      }
-    } catch (e) {
-      console.error('Error parsing user_data:', e);
-      navigate('/');
-    }
-  }, [navigate]);
-    
     if (location.state?.openPopup) {
       setIsVendingRequestPopupOpen(true);  // Open the popup when navigating from the "Add Machine" link
     }
